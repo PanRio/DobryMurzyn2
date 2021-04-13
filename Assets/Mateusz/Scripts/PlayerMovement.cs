efff60d31cf700d9e679f9a28cf6e368492acffb
Mateusz <freedommo33@gmail.com>
2021-04-13 20:50:20 +0200

    Mateusz v.1

--- Assets/Mateusz/Scripts/PlayerMovement.cs
+++ Assets/Mateusz/Scripts/PlayerMovement.cs
@@ -4,16 +4,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float forwardForce = 2000f;
+    public float sidewayForce = 2000f;
     void FixedUpdate() {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(500 * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
         }
     }
 }
---
+++ Assets/Mateusz/Scripts/followPlayer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    void Update() {
+        transform.position = player.position + offset;
+    }
+}
